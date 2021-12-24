1d45ea7de571a64e950b511d8928f8d177f10d98
Mickael Laurent <mlaurent@irif.fr>
2021-12-25 00:17:10 +0100

    add tests for micro.jl

--- test/micro.jl
+++ test/micro.jl
@@ -9,13 +9,16 @@ function test()
     @assert length(ss) == binomial(6+5-1,5)
     total_p = 0
     for (p,s) in ss
+        if s.dice_values[1] == 5
+            @assert p == 1/(6^5)
+        end
         total_p += p
         @assert s.to_draw == 0
         for s2 in g.action1[s].successors
             @assert s2.to_draw >= 0 && s2.to_draw <= 5
         end
     end
-    print(total_p)
+    @assert total_p > 0.9999999 && total_p < 1.0000001
 
     @assert length(g.final) == binomial(6+5-1,5)
     for (k,v) in g.final
